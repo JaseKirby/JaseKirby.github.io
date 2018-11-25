d207e7e4b8395d86727f84ba50e461827212a001
Jase Kirby <jasekirby7@gmail.com>
2018-11-25 15:01:34 -0500

    change to offset columns and better spacing

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -8,17 +8,19 @@ interface ISectionProps {
     bullets?: string[];
 }
 
+const sectionStyle: any = {
+    marginBottom: "20px"
+};
+
 export class Section extends React.Component<ISectionProps> {
     public render(): JSX.Element {
         return(
-            <div className="columns">
+            <div className="columns" style={sectionStyle}>
                 <div className="column">
                     <h3 className="title is-3 has-text-primary">{this.props.organization}</h3>
                     <h5 className="subtitle is-5">{this.props.title}</h5>
                 </div>
-                <div className="column"></div>
-                <div className="column"></div>
-                <div className="column">
+                <div className="column is-offset-6">
                     <h3 className="title is-3 has-text-primary">{this.props.location}</h3>
                     <h5 className="subtitle is-5">{this.props.time}</h5>
                 </div>
